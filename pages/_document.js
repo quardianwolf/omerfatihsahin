a5a926d0929cbcd5c1520b063640895aa8263187
quardianwolf <quardianwolf@gmail.com>
2022-11-15 18:46:01 +0300

    third

--- pages/_document.js
+++ pages/_document.js
@@ -8,15 +8,15 @@ export default function Document() {
       <body className=" home page template-slider button-custom layout-full-width if-zoom if-border-hide no-content-padding no-shadows header-split sticky-header sticky-tb-color ab-hide subheader-both-center menu-link-color menuo-no-borders footer-copy-center mobile-tb-center mobile-side-slide mobile-mini-mr-ll tablet-sticky mobile-header-mini mobile-sticky tr-menu">
         <Main />
         <NextScript />
-        <script src="assets/js/jquery-2.1.4.min.js" strategy="beforeInteractive"></script>
-        <script src="assets/js/mfn.menu.js" strategy="beforeInteractive"></script>
-        <script src="assets/js/jquery.plugins.js" strategy="beforeInteractive"></script>
-        <script src="assets/js/jquery.jplayer.min.js" strategy="beforeInteractive"></script>
-        <script src="assets/js/animations/animations.js" strategy="beforeInteractive"></script>
-        <script src="assets/js/translate3d.js" strategy="beforeInteractive"></script>
-        <script src="assets/js/scripts.js" strategy="beforeInteractive"></script>
-        <script src="assets/rs-plugin-6.custom/js/revolution.tools.min.js" strategy="beforeInteractive"></script>
-        <script src="assets/rs-plugin-6.custom/js/rs6.min.js" strategy="beforeInteractive"></script>
+        <Script src="assets/js/jquery-2.1.4.min.js" strategy="beforeInteractive"></Script>
+        <Script src="assets/js/mfn.menu.js" strategy="beforeInteractive"></Script>
+        <Script src="assets/js/jquery.plugins.js" strategy="beforeInteractive"></Script>
+        <Script src="assets/js/jquery.jplayer.min.js" strategy="beforeInteractive"></Script>
+        <Script src="assets/js/animations/animations.js" strategy="beforeInteractive"></Script>
+        <Script src="assets/js/translate3d.js" strategy="beforeInteractive"></Script>
+        <Script src="assets/js/Scripts.js" strategy="beforeInteractive"></Script>
+        <Script src="assets/rs-plugin-6.custom/js/revolution.tools.min.js" strategy="beforeInteractive"></Script>
+        <Script src="assets/rs-plugin-6.custom/js/rs6.min.js" strategy="beforeInteractive"></Script>
       </body>
     </Html>
   )
