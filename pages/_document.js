5b8d9976f7a2fe95e6a4ded5d2d195c19b3db4f2
quardianwolf <quardianwolf@gmail.com>
2022-11-15 19:00:19 +0300

    dd

--- pages/_document.js
+++ pages/_document.js
@@ -8,15 +8,15 @@ export default function Document() {
       <body className=" home page template-slider button-custom layout-full-width if-zoom if-border-hide no-content-padding no-shadows header-split sticky-header sticky-tb-color ab-hide subheader-both-center menu-link-color menuo-no-borders footer-copy-center mobile-tb-center mobile-side-slide mobile-mini-mr-ll tablet-sticky mobile-header-mini mobile-sticky tr-menu">
         <Main />
         <NextScript />
-        <Script src="assets/js/jquery-2.1.4.min.js"  async></Script>
-        <Script src="assets/js/mfn.menu.js"  async></Script>
-        <Script src="assets/js/jquery.plugins.js"  async></Script>
-        <Script src="assets/js/jquery.jplayer.min.js"  async></Script>
-        <Script src="assets/js/animations/animations.js"  async></Script>
-        <Script src="assets/js/translate3d.js"  async></Script>
-        <Script src="assets/js/Scripts.js"  async></Script>
-        <Script src="assets/rs-plugin-6.custom/js/revolution.tools.min.js"  async></Script>
-        <Script src="assets/rs-plugin-6.custom/js/rs6.min.js"  async></Script>
+        <script src="assets/js/jquery-2.1.4.min.js"  async></script>
+        <script src="assets/js/mfn.menu.js"  async></script>
+        <script src="assets/js/jquery.plugins.js"  async></script>
+        <script src="assets/js/jquery.jplayer.min.js"  async></script>
+        <script src="assets/js/animations/animations.js"  async></script>
+        <script src="assets/js/translate3d.js"  async></script>
+        <script src="assets/js/scripts.js"  async></script>
+        <script src="assets/rs-plugin-6.custom/js/revolution.tools.min.js"  async></script>
+        <script src="assets/rs-plugin-6.custom/js/rs6.min.js"  async></script>
       </body>
     </Html>
   )
