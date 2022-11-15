a3ad33015f45a3a870c55259284f75e99f845e6e
quardianwolf <quardianwolf@gmail.com>
2022-11-15 18:55:04 +0300

    script changed

--- pages/_document.js
+++ pages/_document.js
@@ -8,15 +8,15 @@ export default function Document() {
       <body className=" home page template-slider button-custom layout-full-width if-zoom if-border-hide no-content-padding no-shadows header-split sticky-header sticky-tb-color ab-hide subheader-both-center menu-link-color menuo-no-borders footer-copy-center mobile-tb-center mobile-side-slide mobile-mini-mr-ll tablet-sticky mobile-header-mini mobile-sticky tr-menu">
         <Main />
         <NextScript />
-        <Script src="assets/js/jquery-2.1.4.min.js" strategy="beforeInteractive" defer></Script>
-        <Script src="assets/js/mfn.menu.js" strategy="beforeInteractive" defer></Script>
-        <Script src="assets/js/jquery.plugins.js" strategy="beforeInteractive" defer></Script>
-        <Script src="assets/js/jquery.jplayer.min.js" strategy="beforeInteractive" defer></Script>
-        <Script src="assets/js/animations/animations.js" strategy="beforeInteractive" defer></Script>
-        <Script src="assets/js/translate3d.js" strategy="beforeInteractive" defer></Script>
-        <Script src="assets/js/Scripts.js" strategy="beforeInteractive" defer></Script>
-        <Script src="assets/rs-plugin-6.custom/js/revolution.tools.min.js" strategy="beforeInteractive" defer></Script>
-        <Script src="assets/rs-plugin-6.custom/js/rs6.min.js" strategy="beforeInteractive" defer></Script>
+        <Script src="assets/js/jquery-2.1.4.min.js"  async></Script>
+        <Script src="assets/js/mfn.menu.js"  async></Script>
+        <Script src="assets/js/jquery.plugins.js"  async></Script>
+        <Script src="assets/js/jquery.jplayer.min.js"  async></Script>
+        <Script src="assets/js/animations/animations.js"  async></Script>
+        <Script src="assets/js/translate3d.js"  async></Script>
+        <Script src="assets/js/Scripts.js"  async></Script>
+        <Script src="assets/rs-plugin-6.custom/js/revolution.tools.min.js"  async></Script>
+        <Script src="assets/rs-plugin-6.custom/js/rs6.min.js"  async></Script>
       </body>
     </Html>
   )
